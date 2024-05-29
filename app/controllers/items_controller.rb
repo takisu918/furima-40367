e59715e4ec30b913dbe6802e06dc0494251f34ff
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-05-29 09:11:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     # 必要な処理を書く
   end
+
+  private
+
+  def Item_params
+    params.require(:Item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
