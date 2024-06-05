359c450059d665bd97b53f0ef2cbfa16877aa184
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-06-05 22:52:14 +0900

    商品出品機能実装（追加と修正）

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,11 @@ class Item < ApplicationRecord
   belongs_to :days_until_shipping, class_name: 'DaysUntilShipping'
 
   validates :name, :description, :price, :image, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shipping_fee_payer_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :days_until_shipping_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_fee_payer_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_until_shipping_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
 
 end
