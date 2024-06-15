ae6f85b6be08cfb92107f5090582e4c162e6defd
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-06-15 21:24:44 +0900

    商品情報編集機能（修正2）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    # ロジックは before_action に移動しました
   end
 
   def update
