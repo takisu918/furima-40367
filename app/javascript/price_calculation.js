33b0aa9688d28c8941882cdacfcb7dff72a30a25
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-07-01 11:40:21 +0900

    商品出品機能の実装（最終修正3）

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+document.addEventListener('turbolinks:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
