dda8c1622324c9e33a934295616806aaa9e1364c
takisu918 <ta.kiss.ryokushakeiba0930@gmail.com>
2024-07-01 13:36:06 +0900

    商品出品機能の修正10

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -1,12 +1,15 @@
 window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const profitDom = document.getElementById("profit");
-    let fee = inputValue * 0.1;
-    let profit = inputValue - fee;
-    addTaxDom.innerHTML = Math.floor(fee);
-    profitDom.innerHTML = Math.floor(profit);
-  });
+
+  if (priceInput) {  
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const profitDom = document.getElementById("profit");
+      let fee = inputValue * 0.1;
+      let profit = inputValue - fee;
+      addTaxDom.innerHTML = Math.floor(fee);
+      profitDom.innerHTML = Math.floor(profit);
+    });
+  }
 });
